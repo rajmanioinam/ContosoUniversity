8cffd37f1c3629f93f90eb1ad68ac13c08f0e09a
Aviation1 <Aviation1@AVN-M72853RV>
2017-08-31 21:46:35 -0700

    Add project files.

---
+++ ContosoUniversity/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public abstract class Person
+    {
+        public int ID { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+    }
+}
